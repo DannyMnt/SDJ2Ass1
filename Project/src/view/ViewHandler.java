d4b2de23285f0bb42dc6eb6926fbfc25192ce6cf
DannyMnt <munteanudaniel098@gmail.com>
2024-02-27 10:27:04 +0100

    changes

--- Project/src/view/ViewHandler.java
+++ Project/src/view/ViewHandler.java
@@ -24,7 +24,7 @@ public class ViewHandler
     public void start(Stage primaryStage)
     {
         this.primaryStage = primaryStage;
-        openView("item");
+        openView("library");
     }
 
     public void openView(String id)
