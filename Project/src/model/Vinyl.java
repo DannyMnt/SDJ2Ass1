801421c1c5034f7f78d0190a789efbc24cf903f0
T3Dyy1337 <juricektadeas@gmail.com>
2024-02-29 13:38:35 +0100

    Some model stuff

---
+++ Project/src/model/Vinyl.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Vinyl
+{
+  private String artist;
+  private String title;
+  private int year;
+  private State state;
+
+
+}
---
+++ Project/src/model/VinylList.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.util.ArrayList;
+
+public class VinylList
+{
+  private ArrayList<Vinyl> vinyls;
+
+  public VinylList(ArrayList<Vinyl> vinyls)
+  {
+    this.vinyls = vinyls;
+  }
+
+  public ArrayList<Vinyl> getVinyls()
+  {
+    return vinyls;
+  }
+
+
+  public void addVinyl(Vinyl vinyl){
+    vinyls.add(vinyl);
+  }
+
+
+  public void removeVinyl(Vinyl vinyl){
+    vinyls.remove(vinyl);
+  }
+
+  public Vinyl getVinyl(int index){
+    return vinyls.get(index);
+  }
+}
